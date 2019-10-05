80bcb60280288c53878d063710e91f227a9a8536
nitrid <alikemalkaraca@gmail.com>
2019-10-05 15:17:05 +0300

    .

--- www/_pos/lib/ctrl/Login.js
+++ www/_pos/lib/ctrl/Login.js
@@ -5,12 +5,9 @@ function Login ($scope,$rootScope,$window,db)
     $scope.server_adress = localStorage.host;
     $scope.server_port = localStorage.port;
     $scope.socket_port = localStorage.socketport;        
-    $scope.IsDbCreateWorking = false;
-    $scope.TransferEventProgress = 0;
  
     $scope.Init = function()
     {
-        localStorage.mode = true;
         if (typeof localStorage.host == 'undefined')
         {
             db.SetHost($scope.server_adress,$scope.socket_port);
@@ -19,37 +16,25 @@ function Login ($scope,$rootScope,$window,db)
         
         db.SetHost($scope.server_adress,$scope.socket_port);
 
-        if(localStorage.mode == 'true')
-        {
-            db.Connection(function(data)
-            {                
-                if(data == true)
-                {
-                    $('#alert').alert('close');
-    
-                    db.Emit('QMikroDb',QuerySql.Firma,(data) =>
-                    {
-                        if(typeof data.result.err == 'undefined')
-                        {
-                            $scope.Firm = Firma;
-                            $scope.Kullanici = "";
-                        }
-                    });
-                }
-                else
-                {
-                    $('#alert-box').html('<div class="alert alert-icon alert-danger alert-dismissible" role="alert" id="alert">' +
-                    '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
-                    '<span aria-hidden="true">&times;</span>' +
-                    '</button>' +
-                    '<i class="icon wb-bell" aria-hidden="true"></i> Sunucuya erişim sağlanamadı.' +
-                    '<p class="mt-15">' +
-                    '<button class="btn btn-primary" data-target="#server-settings" data-toggle="modal"' +
-                    'type="button">Ayarlar</button></p></div>');
-                    db.Disconnect();
-                }
-            });
-        }
+        db.Connection(function(data)
+        {                
+            if(data == true)
+            {
+                $('#alert').alert('close');
+            }
+            else
+            {
+                $('#alert-box').html('<div class="alert alert-icon alert-danger alert-dismissible" role="alert" id="alert">' +
+                '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
+                '<span aria-hidden="true">&times;</span>' +
+                '</button>' +
+                '<i class="icon wb-bell" aria-hidden="true"></i> Sunucuya erişim sağlanamadı.' +
+                '<p class="mt-15">' +
+                '<button class="btn btn-primary" data-target="#server-settings" data-toggle="modal"' +
+                'type="button">Ayarlar</button></p></div>');
+                db.Disconnect();
+            }
+        });
     }
     $scope.HostSettingSave = function()
     {
@@ -106,10 +91,5 @@ function Login ($scope,$rootScope,$window,db)
             }
             $scope.$apply();            
         });
-    }
-    $scope.ChangeMode = function()
-    {
-        localStorage.mode = document.getElementById('inputBasicOn').checked;
-        $window.location.reload();
     }
 }
