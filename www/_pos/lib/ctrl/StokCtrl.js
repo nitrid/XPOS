dc0748b8b13be08f64e8981742aec5b59707af1a
nitrid <alikemalkaraca@gmail.com>
2019-10-19 19:45:55 +0300

    .

--- www/_pos/lib/ctrl/StokCtrl.js
+++ www/_pos/lib/ctrl/StokCtrl.js
@@ -2,6 +2,42 @@ function StokCtrl ($scope,$window,db)
 {
     $scope.Init = function()
     {
-
+        $("#TblFiyat").DataTable(
+            {
+                "pageLength": 5,
+                "lengthChange": false,
+                ordering:false,
+                info:false,
+                searching:false
+            }
+        );
+    }
+    $scope.BtnTabFiyat = function()
+    {
+        $("#TabFiyat").addClass('active');
+        $("#TabBirim").removeClass('active');
+        $("#TabBarkod").removeClass('active');
+        $("#TabTedarikci").removeClass('active');
+    }
+    $scope.BtnTabBirim = function()
+    {
+        $("#TabBirim").addClass('active');
+        $("#TabFiyat").removeClass('active');
+        $("#TabBarkod").removeClass('active');
+        $("#TabTedarikci").removeClass('active');
+    }
+    $scope.BtnTabBarkod = function()
+    {
+        $("#TabBarkod").addClass('active');
+        $("#TabFiyat").removeClass('active');
+        $("#TabBirim").removeClass('active');
+        $("#TabTedarikci").removeClass('active');
+    }
+    $scope.BtnTabTedarikci = function()
+    {
+        $("#TabTedarikci").addClass('active');
+        $("#TabFiyat").removeClass('active');
+        $("#TabBarkod").removeClass('active');
+        $("#TabBirim").removeClass('active');
     }
 }
