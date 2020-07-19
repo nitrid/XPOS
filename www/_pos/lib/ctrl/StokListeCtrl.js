318104789c55f38f80e880aebc547cb7ff8af86c
nitrid <alikemalkaraca@gmail.com>
2020-07-19 19:10:22 +0300

    .

--- www/_pos/lib/ctrl/StokListeCtrl.js
+++ www/_pos/lib/ctrl/StokListeCtrl.js
@@ -124,7 +124,7 @@ function StokListeCtrl ($scope,$window,db)
             selecting: true,
             data : $scope.Data,
             paging : true,
-            pageSize: 20,
+            pageSize: 100,
             pageButtonCount: 5,
             pagerFormat: "{pages} {next} {last}    {pageIndex} of {pageCount}",
             fields: TmpFields
