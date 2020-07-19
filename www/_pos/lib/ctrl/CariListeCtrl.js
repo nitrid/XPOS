ea88e8e2a2550ef711bfdb95902c1bbcccb9775d
nitrid <alikemalkaraca@gmail.com>
2020-07-19 13:00:37 +0300

    .

--- www/_pos/lib/ctrl/CariListeCtrl.js
+++ www/_pos/lib/ctrl/CariListeCtrl.js
@@ -1,37 +1,212 @@
 function CariListeCtrl ($scope,$window,db)
 {
-    $scope.Init = function()
+    let TmpFields =
+    [
+        {
+            name: "CODE",
+            title : "CODE",
+            type : "text",
+            align: "center",
+            width: 100,
+            itemTemplate: function(value) 
+            {
+                return $("<a>").attr("href", "#!Cari?Id='" + value + "'").text(value);
+            }            
+        },
+        {
+            name: "NAME",
+            title : "NAME",
+            align: "center",
+            width: 100
+        },
+        {
+            name: "GENUS",
+            title : "GENUS",
+            align: "center",
+            width: 75,
+            visible: false
+        },
+        {
+            name: "LAST_NAME",
+            title : "LAST NAME",
+            align: "center",
+            width: 75,
+            visible: false
+        },
+        {
+            name: "COMPANY",
+            title : "COMPANY",
+            align: "center",
+            width: 75,
+            visible: false
+        },
+        {
+            name: "CUSTOMER_GRP",
+            title : "CUSTOMER GRP",
+            align: "center",
+            width: 75,
+            visible: false
+        },
+        {
+            name: "PHONE1",
+            title : "PHONE1",
+            align: "center",
+            width: 75,
+            visible: false
+        },
+        {
+            name: "PHONE2",
+            title : "PHONE2",
+            align: "center",
+            width: 75,
+            visible: false
+        },
+        {
+            name: "GSM_PHONE",
+            title : "GSM_PHONE",
+            align: "center",
+            width: 75,
+            visible: false
+        },
+        {
+            name: "OTHER_PHONE",
+            title : "OTHER_PHONE",
+            align: "center",
+            width: 75,
+            visible: false
+        },
+        {
+            name: "EMAIL",
+            title : "EMAIL",
+            align: "center",
+            width: 75,
+            visible: false
+        },
+        {
+            name: "WEB",
+            title : "WEB",
+            align: "center",
+            width: 75,
+            visible: false
+        },
+        {
+            name: "SIRET_ID",
+            title : "SIRET_ID",
+            align: "center",
+            width: 75,
+            visible: false
+        },
+        {
+            name: "APE_CODE",
+            title : "APE_CODE",
+            align: "center",
+            width: 75,
+            visible: false
+        },
+        {
+            name: "TAX_OFFICE",
+            title : "TAX_OFFICE",
+            align: "center",
+            width: 75,
+            visible: false
+        },
+        {
+            name: "TAX_NO",
+            title : "TAX_NO",
+            align: "center",
+            width: 75,
+            visible: false
+        }
+    ];
+    function TblCariInit()
+    {
+        $("#TblCari").jsGrid
+        ({
+            width: "100%",
+            updateOnResize: true,
+            heading: true,
+            selecting: true,
+            data : $scope.Data,
+            paging : true,
+            pageSize: 20,
+            pageButtonCount: 5,
+            pagerFormat: "{pages} {next} {last}    {pageIndex} of {pageCount}",
+            fields: TmpFields
+        });
+    }
+    function CariGetir()
     {
         let TmpQuery = 
         {
             db : $scope.Firma,
-            query:  "SELECT [CODE],[NAME],[LAST_NAME] FROM CUSTOMERS"
+            query:  "SELECT " + 
+                    "CODE AS CODE, " + 
+                    "GENUS AS GENUS, " + 
+                    "NAME AS NAME, " + 
+                    "LAST_NAME AS LAST_NAME, " + 
+                    "COMPANY AS COMPANY, " + 
+                    "CUSTOMER_GRP AS CUSTOMER_GRP, " + 
+                    "PHONE1 AS PHONE1, " + 
+                    "PHONE2 AS PHONE2, " + 
+                    "GSM_PHONE AS GSM_PHONE, " + 
+                    "OTHER_PHONE AS OTHER_PHONE, " + 
+                    "EMAIL AS EMAIL, " + 
+                    "WEB AS WEB, " + 
+                    "NOTE AS NOTE, " + 
+                    "SIRET_ID AS SIRET_ID, " + 
+                    "APE_CODE AS APE_CODE, " + 
+                    "TAX_OFFICE AS TAX_OFFICE, " + 
+                    "TAX_NO AS TAX_NO, " + 
+                    "INT_VAT_NO AS INT_VAT_NO " + 
+                    "FROM CUSTOMERS WHERE ((CODE = @CODE) OR (@CODE = '')) AND ((NAME = @NAME) OR (@NAME = ''))",
+            param : ["CODE:string|50","NAME:string|250"],
+            value : [$scope.Kodu,$scope.Adi,]
         }
+
         db.GetDataQuery(TmpQuery,function(Data)
         {
-            $('#Table').DataTable( {
-                lengthChange: false,
-                pageLength: 50,
-                data: Data,
-                columns: 
-                [
-                    { data: "CODE",width:"100" },
-                    { data: "NAME",width:"200" },
-                    { data: "LAST_NAME",width:"100" }
-                ],
-                columnDefs: 
-                [ 
-                    {
-                        targets: 0,
-                        data: "CODE",
-                        render : function ( data, type, row, meta ) 
-                        {
-                            return '<a href="#!Cari?Id='+data+'">' + data + '</a>';
-                        }
-                    } 
-                ]
-            } );
+            console.log(Data)
+            $scope.Data = Data
+            $("#TblCari").jsGrid({data : $scope.Data});
         });
-        
+    }
+    $scope.Init = function()
+    {
+        $scope.Data = [];
+
+        $scope.Kolon = ["CODE","NAME"];
+        $scope.Kodu = "";
+        $scope.Adi = "";
+
+        TblCariInit();
+
+        setTimeout(function () 
+        {
+            $('select').selectpicker('refresh');
+        },500)
+    }
+    $scope.KolonChange = function()
+    {
+        for (let i = 0; i < TmpFields.length; i++) 
+        {
+            TmpFields[i].visible = false;
+        }
+
+        for (let i = 0; i < TmpFields.length; i++) 
+        {
+            for (let x = 0; x < $scope.Kolon.length; x++) 
+            {
+                if($scope.Kolon[x] == TmpFields[i].name)    
+                {
+                    TmpFields[i].visible = true;
+                }                
+            }            
+        }
+
+        TblCariInit();
+    }
+    $scope.BtnAra = function()
+    {
+        CariGetir();
     }
 }
--- www/_pos/lib/ctrl/StokListeCtrl.js
+++ www/_pos/lib/ctrl/StokListeCtrl.js
@@ -1,5 +1,96 @@
 function StokListeCtrl ($scope,$window,db)
 {
+    let TmpFields =
+    [
+        {
+            name: "CODE",
+            title : "CODE",
+            type : "text",
+            align: "center",
+            width: 100,
+            itemTemplate: function(value) 
+            {
+                return $("<a>").attr("href", "#!Stok?Id='" + value + "'").text(value);
+            }            
+        },
+        {
+            name: "NAME",
+            title : "NAME",
+            align: "center",
+            width: 100
+        },
+        {
+            name: "SNAME",
+            title : "SNAME",
+            align: "center",
+            width: 75,
+            visible: false
+        },
+        {
+            name: "ITEM_GRP",
+            title : "ITEM GRP",
+            align: "center",
+            width: 75,
+            visible: false
+        },
+        {
+            name: "VAT",
+            title : "VAT",
+            align: "center",
+            width: 75,
+            visible: false
+        },
+        {
+            name: "COST_PRICE",
+            title : "COST PRICE",
+            align: "center",
+            width: 75,
+            visible: false
+        },
+        {
+            name: "MIN_PRICE",
+            title : "MIN PRICE",
+            align: "center",
+            width: 75,
+            visible: false
+        },
+        {
+            name: "MAX_PRICE",
+            title : "MAX PRICE",
+            align: "center",
+            width: 75,
+            visible: false
+        },
+        {
+            name: "UNIT",
+            title : "UNIT",
+            align: "center",
+            width: 75,
+            visible: false
+        },
+        {
+            name: "BARCODE",
+            title : "BARCODE",
+            align: "center",
+            width: 75,
+            visible: false
+        },
+        {
+            name: "PRICE",
+            title : "PRICE",
+            align: "center",
+            width: 75,
+            visible: false
+        },
+        {
+            name: "CUSTOMER_ITEM_CODE",
+            title : "CUSTOMER CODE",
+            align: "center",
+            width: 75,
+            visible: false
+        }
+    ];
+
     function TblStokInit()
     {
         $("#TblStok").jsGrid
@@ -13,128 +104,110 @@ function StokListeCtrl ($scope,$window,db)
             pageSize: 20,
             pageButtonCount: 5,
             pagerFormat: "{pages} {next} {last}    {pageIndex} of {pageCount}",
-            fields: 
-            [
-                {
-                    name: "CODE",
-                    title : "KODU",
-                    type : "text",
-                    align: "center",
-                    width: 100
-                    
-                },
-                {
-                    name: "NAME",
-                    title : "ADI",
-                    align: "center",
-                    width: 100
-                },
-                {
-                    name: "TYPE",
-                    title : "Tip",
-                    align: "center",
-                    width: 75
-                },
-                { type: "control", modeSwitchButton: true , editButton: false }  
-            ]
+            fields: TmpFields
         });
     }
+    function StokGetir()
+    {
+        let TmpVal = ""
+        for (let i = 0; i < $scope.Barkod.split(',').length; i++) 
+        {
+            TmpVal += "'" + $scope.Barkod.split(',')[i] + "'"
+            if($scope.Barkod.split(',').length > 1 && i !=  ($scope.Barkod.split(',').length - 1))
+            {
+                TmpVal += ","
+            }
+        }
+
+        let TmpQuery = 
+        {
+            db : $scope.Firma,
+            query:  "SELECT " +
+                    "ITEMS.CODE AS CODE, " +
+                    "ITEMS.NAME AS NAME, " +
+                    "ITEMS.SNAME AS SNAME, " +
+                    "ITEMS.ITEM_GRP AS ITEM_GRP, " +
+                    "ITEMS.VAT AS VAT, " +
+                    "ITEMS.COST_PRICE AS COST_PRICE, " +
+                    "ITEMS.MIN_PRICE AS MIN_PRICE, " +
+                    "ITEMS.MAX_PRICE AS MAX_PRICE, " +
+                    "ISNULL(ITEM_UNIT.NAME,'') AS UNIT, " +
+                    "ISNULL(ITEM_BARCODE.BARCODE,'') AS BARCODE, " +
+                    "ISNULL((SELECT PRICE FROM ITEM_PRICE WHERE ITEM_CODE = ITEMS.CODE AND QUANTITY = 1),0) AS PRICE, " +
+                    "ITEM_CUSTOMER.CUSTOMER_ITEM_CODE AS CUSTOMER_ITEM_CODE " +
+                    "FROM ITEMS " +
+                    "LEFT OUTER JOIN ITEM_UNIT ON " +
+                    "ITEMS.CODE = ITEM_UNIT.ITEM_CODE " +
+                    "LEFT OUTER JOIN ITEM_BARCODE ON " +
+                    "ITEM_BARCODE.ITEM_CODE = ITEMS.CODE AND ITEM_BARCODE.UNIT = ITEM_UNIT.GUID " +
+                    "LEFT OUTER JOIN ITEM_CUSTOMER ON " +
+                    "ITEM_CUSTOMER.ITEM_CODE = ITEMS.CODE " +
+                    "WHERE ((ITEM_BARCODE.BARCODE IN (" + TmpVal + ") OR ITEMS.CODE IN (" + TmpVal + ") OR " + 
+                    "ITEM_CUSTOMER.CUSTOMER_ITEM_CODE IN (" + TmpVal + ")) OR (@BARCODE = '')) AND ((ITEMS.NAME LIKE @NAME + '%') OR (@NAME = '')) AND " +
+                    "((ITEMS.ITEM_GRP = @ITEM_GRP) OR (@ITEM_GRP = '')) AND ITEMS.STATUS = @STATUS",
+            param : ["BARCODE:string|50","NAME:string|250","ITEM_GRP:string|25","STATUS:bit"],
+            value : [$scope.Barkod,$scope.Adi,$scope.Grup,$scope.Durum]
+        }
+
+        db.GetDataQuery(TmpQuery,function(Data)
+        {
+            $scope.Data = Data
+            $("#TblStok").jsGrid({data : $scope.Data});
+        });
+    }
+    $scope.GrupGetir = function()
+    {
+        let TmpQuery = 
+        {
+            db : $scope.Firma,
+            query:  "SELECT [NAME],[CODE] FROM ITEM_GROUP"
+        }
+        db.GetDataQuery(TmpQuery,function(Data)
+        {
+            $scope.GrupList = Data
+        });
+    } 
     $scope.Init = function()
     {
+        $scope.Data = [];
+        $scope.GrupList = [];
+
+        $scope.Kolon = ["CODE","NAME"];
+        $scope.Barkod = "";
+        $scope.Adi = "";
+        $scope.Grup = "";
+        $scope.Durum = true;
+
         TblStokInit();
-        // let TmpQuery = 
-        // {
-        //     db : $scope.Firma,
-        //     query:  "SELECT [CODE],[NAME],[SNAME],[ITEM_GRP] FROM ITEMS"
-        // }
-        // db.GetDataQuery(TmpQuery,function(Data)
-        // {
-        //     $scope.TableData = Data
-        //     $scope.TableCreate();
-        // });
-        // $scope.GroupSelect = '0';
-        // $scope.GroupRead()
+        $scope.GrupGetir();
+
+        setTimeout(function () 
+        {
+            $('select').selectpicker('refresh');
+        },500)
     }
-    // $scope.GroupRead = function()
-    // {
-    //     let TmpQuery = 
-    //     {
-    //         db : $scope.Firma,
-    //         query:  "SELECT [NAME],[CODE] FROM ITEM_GROUP"
-    //     }
-    //     db.GetDataQuery(TmpQuery,function(Data)
-    //     {
-    //         $scope.GroupList = Data
-    //     });
-    // } 
-    // $scope.CmbGroupChange = function()
-    // {
-    //     console.log($scope.GroupSelect)
-    //     if($scope.GroupSelect == '0')
-    //     {
-    //         let TmpQuery = 
-    //         {
-    //             db : $scope.Firma,
-    //             query:  "SELECT [CODE],[NAME],[SNAME],[ITEM_GRP] FROM ITEMS"
-    //         }
-    //         db.GetDataQuery(TmpQuery,function(Data)
-    //         {
-    //             $scope.TableData = Data
-    //             $scope.TableCreate();
-    //         });
-    //     }
-    //     else
-    //     {
-    //         let TmpQuery = 
-    //         {
-    //             db : $scope.Firma,
-    //             query:  "SELECT [CODE],[NAME],[SNAME],[ITEM_GRP] FROM ITEMS WHERE ITEM_GRP =  '" + $scope.GroupSelect + "'"
-    //         }
-    //         db.GetDataQuery(TmpQuery,function(Data)
-    //         {
-    //             console.log(Data)
-    //             $scope.TableData = Data
-    //             $scope.TableCreate();
-    //         });
-    //     }
-       
-        
-    // }
-    // $scope.TableCreate = function()
-    // {
-    //     $('#Table').DataTable( {
-    //         destroy: true,
-    //         paging: true,
-    //         lengthChange: false,
-    //         pageLength: 50,
-    //         searching: true,
-    //         data: $scope.TableData,
-    //         columns: 
-    //         [
-    //             { data: "CODE",width:"100" },
-    //             { data: "NAME",width:"200" },
-    //             { data: "SNAME",width:"100" },
-    //             { data: "ITEM_GRP",width:"100"}
-    //         ],
-    //         columnDefs: 
-    //         [ 
-    //             {
-    //                 targets: 0,
-    //                 data: "NAME",
-    //                 render : function ( data, type, row, meta ) 
-    //                 {
-    //                     return '<a href="#!Stok?Id='+data+'">' + data + '</a>';
-    //                 }
-    //             }, 
-    //             {
-    //                 targets: 1,
-    //                 data: "CODE",
-    //                 render : function ( data, type, row, meta ) 
-    //                 {
-    //                     return '<a href="#!Stok?Id='+row.CODE+'">' + data + '</a>';
-    //                 }
-    //             },
-    //         ]
-    //     } );
-    // }
+    $scope.KolonChange = function()
+    {
+        for (let i = 0; i < TmpFields.length; i++) 
+        {
+            TmpFields[i].visible = false;
+        }
+
+        for (let i = 0; i < TmpFields.length; i++) 
+        {
+            for (let x = 0; x < $scope.Kolon.length; x++) 
+            {
+                if($scope.Kolon[x] == TmpFields[i].name)    
+                {
+                    TmpFields[i].visible = true;
+                }                
+            }            
+        }
+
+        TblStokInit();
+    }
+    $scope.BtnAra = function()
+    {
+        StokGetir();
+    }
 }
