6351a114067436934cc2c871929caa66c5a0d54c
nitrid <alikemalkaraca@gmail.com>
2020-09-13 16:36:00 +0300

    .

--- www/js/MettlerScale.js
+++ www/js/MettlerScale.js
@@ -67,6 +67,8 @@ var MettlerScale =
                     ReciveBuffer += line.toString()
                     if(ReciveBuffer.length >= 26)
                     {
+                        let TmpScale = ReciveBuffer.substring(5,11)
+                        console.log(TmpScale)
                         console.log(ReciveBuffer)
                         port.close();
                     }
