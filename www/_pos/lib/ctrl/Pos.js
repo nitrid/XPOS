d2b3ea806f51281309ac80d6f6eee13da5ff63c5
nitrid <alikemalkaraca@gmail.com>
2021-01-13 03:11:53 +0300

    .

--- www/_pos/lib/ctrl/Pos.js
+++ www/_pos/lib/ctrl/Pos.js
@@ -4268,9 +4268,11 @@ function Pos($scope,$window,$rootScope,db)
                 }
                         
                 await db.GetPromiseQuery(TmpQuery)
-                let TmpData = await db.GetPromiseTag($scope.Firma,'PosSatisGetir',[$scope.Sube,$scope.EvrakTip,$scope.Seri,$scope.Sira])
-                console.log(TmpData)
-                InsertSonYenile(TmpData)
+                db.GetData($scope.Firma,'PosSatisGetir',[$scope.Sube,$scope.EvrakTip,$scope.Seri,$scope.Sira],()=>
+                {
+                    console.log(TmpData)
+                    InsertSonYenile(TmpData)
+                })
             }
             
         }
