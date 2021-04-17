87504e87444736d92705875706541383464f2954
nitrid <alikemalkaraca@gmail.com>
2021-04-18 01:55:08 +0300

    .

--- www/js/DbService.js
+++ www/js/DbService.js
@@ -957,11 +957,11 @@ angular.module('app.db', []).service('db',function($rootScope)
             TmpData.push({font:"b",style:"b",align:"lt",data:_PrintText("****************************************************************",64)});
         }
         
-        TmpData.push({font:"b",style:"b",align:"lt",data:_PrintText(" ",64)});
+        //TmpData.push({font:"b",style:"b",align:"lt",data:_PrintText(" ",64)});
         //if(_Equal(pTData,"TYPE",4) && _Equal(pTData,"DOC_TYPE",1))
         if(pParamData[5] != '')
         {
-            TmpData.push({font:"b",style:"b",align:"ct",size : [1,1],data:"Rest Bon d'avoir : " + parseFloat(pParamData[5].substring(8,12) / 100).toFixed(2) + "€"});
+            TmpData.push({font:"b",style:"b",align:"ct",size : [1,0],data:"Rest Bon d'avoir : " + parseFloat(pParamData[5].substring(8,12) / 100).toFixed(2) + "EUR"});
             TmpData.push({align:"ct",barcode:pParamData[5],options:{width: 1,height:90}});
             TmpData.push({font:"b",style:"b",align:"lt",data:_PrintText(" ",64)});
             TmpData.push({font:"b",style:"b",align:"ct",data:"Avoir valable 3 mois apres edition..."});
