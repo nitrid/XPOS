5b7d4f34ea444ebdd3837639234b188bccf69740
piqpos <piqposfr@gmail.com>
2021-06-29 22:59:16 +0200

    .

--- www/lib/ctrl/Main.js
+++ www/lib/ctrl/Main.js
@@ -3,6 +3,15 @@ function Main ($scope,$rootScope,$window,db)
     let TmpActiveTime;
     function MenuOlustur()
     {   
+        if(typeof localStorage.Lang != 'undefined')
+        {
+            $scope.Lang = localStorage.Lang;
+        }
+        else
+        {
+            $scope.Lang = "TR";
+        } 
+
         let HtmlText = "";
 
         $scope.User = "0";
@@ -11,18 +20,18 @@ function Main ($scope,$rootScope,$window,db)
         {
             HtmlText = HtmlText + "<a>";
             HtmlText = HtmlText + "<i class='site-menu-icon wb-settings'></i>";
-            HtmlText = HtmlText + "<span class='site-menu-title' langu>Ayarlar</span>";
+            HtmlText = HtmlText + "<span class='site-menu-title'>" + db.Language($scope.Lang,"Ayarlar") + "</span>";
             HtmlText = HtmlText + "<span class='site-menu-arrow'></span>";
             HtmlText = HtmlText + "</a>";
             HtmlText = HtmlText + "<ul class='site-menu-sub'>";
             HtmlText = HtmlText + "<li class='site-menu-item'>"
             HtmlText = HtmlText + "<a class='animsition-link' href='#!KullaniciParametre'>";
-            HtmlText = HtmlText + "<span class='site-menu-title' langu>Kullanıcı Tanımlama</span>";
+            HtmlText = HtmlText + "<span class='site-menu-title'>" + db.Language($scope.Lang,"Kullanıcı Tanımlama") + "</span>";
             HtmlText = HtmlText + "</a>";
             HtmlText = HtmlText + "</li>";
             HtmlText = HtmlText + "<li class='site-menu-item'>"
             HtmlText = HtmlText + "<a class='animsition-link' href='#!CihazParametre'>";
-            HtmlText = HtmlText + "<span class='site-menu-title' langu>Cihaz Tanımlama</span>";
+            HtmlText = HtmlText + "<span class='site-menu-title'>" + db.Language($scope.Lang,"Cihaz Tanımlama") + "</span>";
             HtmlText = HtmlText + "</a>";
             HtmlText = HtmlText + "</li>";
             HtmlText = HtmlText + "</ul>";
