d560175ffd3f84b4f57240d2274ac5ecc3069bf9
nitrid <alikemalkaraca@gmail.com>
2021-07-09 23:43:48 +0300

    .

--- www/lib/ctrl/EtiketBasimCtrl.js
+++ www/lib/ctrl/EtiketBasimCtrl.js
@@ -181,6 +181,12 @@ function EtiketBasimCtrl ($scope,$window,db)
                     width: "100"
                 },
                 {
+                    dataField: "CUSTOMER_ITEM_CODE",
+                    caption: db.Language($scope.Lang,"TEDARİKÇİ KODU"),
+                    dataType: "string",
+                    width: "100"
+                },
+                {
                     dataField: "BARCODE",
                     caption: db.Language($scope.Lang,"BARKOD"),
                     dataType: "string",
@@ -335,6 +341,7 @@ function EtiketBasimCtrl ($scope,$window,db)
                         "CONVERT(NVARCHAR,MAX(FACTOR)) + ' ' + MAX(SHORT) AS UNDER_UNIT_VALUE, " +
                         "CASE WHEN dbo.FN_PRICE_SALE(ITEMS.CODE,1,GETDATE()) = 0 OR ISNULL(MAX(FACTOR),0) = 0 THEN '0' ELSE " +
                         "ROUND(dbo.FN_PRICE_SALE(ITEMS.CODE,1,GETDATE()) / ISNULL(MAX(FACTOR),0),2) END AS UNDER_UNIT_PRICE, " +
+                        "ITEM_CUSTOMER.CUSTOMER_ITEM_CODE AS CUSTOMER_ITEM_CODE, " +
                         "'' AS DESCRIPTION " +
                         "FROM ITEMS " +
                         "LEFT OUTER JOIN ITEM_BARCODE ON " +
