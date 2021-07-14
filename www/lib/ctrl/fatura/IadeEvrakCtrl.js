e34b9f4aca38e75df7ba87e7d0e72136f6a42c7f
nitrid <alikemalkaraca@gmail.com>
2021-07-14 16:02:11 +0300

    .

--- www/lib/ctrl/fatura/IadeEvrakCtrl.js
+++ www/lib/ctrl/fatura/IadeEvrakCtrl.js
@@ -431,7 +431,7 @@ function IadeEvrakCtrl ($scope,$window,$timeout,$location,db)
                 db : $scope.Firma,
                 query:  "SELECT *, " +
                         "ISNULL((SELECT TOP 1 COST_PRICE FROM ITEMS WHERE CODE = INVOICE_VW_01.ITEM_CODE),0) AS COST_PRICE, " + 
-                        "ISNULL((SELECT VAT FROM ITEMS WHERE CODE = ITEM_CODE),0) AS VATRATE " +
+                        "ISNULL((SELECT CUSTOMER_ITEM_CODE FROM ITEM_CUSTOMER WHERE ITEM_CUSTOMER.ITEM_CODE = INVOICE_VW_01.ITEM_CODE AND ITEM_CUSTOMER.CUSTOMER_CODE = INVOICE_VW_01.CUSTOMER),'') AS CUSTOMER_ITEM_CODE " +
                         "FROM INVOICE_VW_01 WHERE REF = @REF AND REF_NO = @REF_NO AND TYPE = @TYPE AND DOC_TYPE = @DOC_TYPE ORDER BY LINE_NO DESC",
                 param:  ['REF','REF_NO','DOC_TYPE','TYPE'],
                 type:   ['string|25','int','int','int'],
