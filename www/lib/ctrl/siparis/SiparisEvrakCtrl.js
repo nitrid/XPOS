2b0d247c9ff08c156b83f7706194d66a5e2d9f52
nitrid <alikemalkaraca@gmail.com>
2021-10-03 14:17:03 +0300

    .

--- www/lib/ctrl/siparis/SiparisEvrakCtrl.js
+++ www/lib/ctrl/siparis/SiparisEvrakCtrl.js
@@ -281,7 +281,7 @@ function SiparisEvrakCtrl ($scope,$window,$timeout,$location,db)
             }
         });
     }
-    function InitEvrakGrid(pPage)
+    function InitEvrakGrid()
     {
         $("#TblEvrak").jsGrid
         ({
@@ -292,7 +292,7 @@ function SiparisEvrakCtrl ($scope,$window,$timeout,$location,db)
             heading: true,
             selecting: true,
             data : $scope.EvrakListe,
-            paging : pPage,
+            paging : 10,
             pageSize: 10,
             pageButtonCount: 3,
             pagerFormat: "{pages} {next} {last}    {pageIndex} of {pageCount}",
